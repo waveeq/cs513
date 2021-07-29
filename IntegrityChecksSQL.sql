05da15dfbcdda58b564add7dcbd9d9e681167e63
Daniel Crosson <40742799+dcross42@users.noreply.github.com>
2021-07-28 22:42:23 -0400

    updated sql queries and split into profiling and constraint checking

--- IntegrityChecksSQL.sql
+++ IntegrityChecksSQL.sql
@@ -2,25 +2,30 @@
 
 /* DISH */
 
+/* Check that each id is unique */
 select id, count(id) as cnt
 from dish
 group by id
 having count(id)  > 1;
 
-
+/* check that there are no null ids */
 select * from dish where id is NULL;
 
+/* check that there are no null appearances */
 select * from dish
-where menus_appeared is NULL or times_appeared is NULL ;
+where menus_appeared is NULL or times_appeared is NULL;
 
 select * from dish
 where cast(first_appeared as year) > cast(last_appeared as year) and last_appeared <> '0';
 
-
 select * from dish where (first_appeared between '1851' and '2021');
-select * from dish where (lasst_appeared between '1851' and '2021');
+select * from dish where (last_appeared between '1851' and '2021');
+
 
 select * from dish where lowest_price > highest_price;
+
+select count(*) from dish where lowest_price < 0;
+select count(*) from dish where highest_price < 0;
 
 
 /* MENU */
@@ -32,40 +37,35 @@ having count(id)  > 1;
 
 select * from menu where id is NULL;
 
-select * from menu where sponsor_clean is NULL or '';
 select * from menu where page_count is NULL or '';
-
-
 
 /* MENU PAGE */
 
 select id, count(id) as cnt
-from page
+from menupage
 group by id
 having count(id)  > 1;
 
 
-select * from page where id is NULL;
-
-select * from page where page_number  = '0';
-select * from page where page_number  is NULL;
-
-
-select * from page where created_at > updated_at;
-
+select * from menupage where id is NULL;
+select * from menupage where menu_id is NULL;
 
 /* ITEM */
 
 select id, count(id) as cnt
-from item
+from menuitem
 group by id
 having count(id)  > 1;
 
-select * from item where id is NULL;
+select * from menuitem where id is NULL;
 
-select * from item where updated_at < created_at;
+select *
+from menuitem where dish_id is NULL;
 
-select * from item where xpos < 0 and xpos > 1;
-select * from item where ypos < 0 and xpos > 1;
+select * from menuitem where updated_at < created_at;
 
+select * from menuitem where price < 0;
+
+select * from menuitem where xpos < 0 and xpos > 1;
+select * from menuitem where ypos < 0 and xpos > 1;
 
